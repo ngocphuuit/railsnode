faaf2b0206226c9ea1f9ebb3cb7e992d89c550a8
Administrator <admin@example.com>
2015-04-06 17:02:48 +0700

    redirect 404 page

--- railsapp/app/controllers/application_controller.rb
+++ railsapp/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def render_optional_error_file(status_code)
+		if status_code == :not_found
+			render_404
+		else
+			super
+		end
+	end
 end
--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'errors/file_not_found'
+
+  get 'errors/unprocessable'
+
+  get 'errors/internal_server_error'
+
+	match '/404', to: 'errors#file_not_found', via: :all
 	get 'home' => 'realtimes#index'
 	post 'chatroom' => 'realtimes#chatroom'
 end
